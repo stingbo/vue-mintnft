812709dc7bab5a05e3bc1e3dc2db67fb4d14f5c3
sting <lianbo.wan@gmail.com>
2022-06-19 21:17:09 +0800

    完成页面展示

---
+++ src/common/Network.vue
@@ -0,0 +1,14 @@
+<script>
+const NETWORK_MAP = {
+    '1': '主网',
+    '3': 'Ropsten Test Network',
+    '4': 'Rinkeby Test Network',
+    '5': 'Goerli Test Network',
+    '42': 'Kovan Test Network',
+}
+
+export default {
+  name: "network-map",
+  NETWORK_MAP,
+};
+</script>
--- src/components/MintNFT.vue
+++ src/components/MintNFT.vue
@@ -1,4 +1,24 @@
 <template>
+  <div>
+    <span>账号：{{ account }}</span>
+  </div>
+  <div>
+    <span>网络：{{ network }}</span>
+  </div>
+  <div>
+    <span>合约地址：
+        <a :href="'https://rinkeby.etherscan.io/address/'+contractAddress" target="_blank">
+        {{ contractAddress }}
+        </a>
+    </span>
+  </div>
+  <div v-if="isTransaction">
+    <span>交易hash值：
+        <a :href="'https://rinkeby.etherscan.io/tx/'+transactionHash" target="_blank">
+        {{ transactionHash }}
+        </a>
+    </span>
+  </div>
   <div class="submit-form">
       <button @click="mintNFT" class="btn btn-success">铸造</button>
   </div>
@@ -6,63 +26,69 @@
 
 <script>
 import Web3 from 'web3';
+import Network from '../common/Network'
 
 export default {
   name: "mint-NFT",
   data() {
     return {
+        account: "",
+        network: "",
+        contractAddress: "",
+        transactionHash: "",
+        isTransaction: 0,
     };
   },
   methods: {
     async mintNFT() {
         this.isEnableEth();
 
-        const web3 = new Web3(window.ethereum);
         try {
-            let accounts = await web3.eth.getAccounts();
-            console.log(accounts);
-            let account = accounts[0];
-            console.log("地址:"+account);
-            let balance = await web3.eth.getBalance(account);
-            console.log("账户余额:"+balance)
-
-            web3.eth.getChainId().then(console.log);
+            const web3 = new Web3(window.ethereum);
 
             const contract = require("../../contract.json");
-            const contractAddress = "0x35907b3D73F3bbC391344eF56d7a552f4B2d390c";
-            const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+            const nftContract = new web3.eth.Contract(contract.abi, this.contractAddress);
 
             // await web3.eth.sendTransaction({
-            //     from: account,
+            //     from: this.account,
             //     to: '0x4063cf0060Da8b3e18F848AeAA52334cbcE9F662',
             //     value: web3.utils.toWei("0.0001")
             // })
-            const nonce = await web3.eth.getTransactionCount(account, "latest"); //get latest nonce
+            const nonce = await web3.eth.getTransactionCount(this.account, "latest"); //get latest nonce
+            // this.isTransaction = 1;
 
             //the transaction
             const tx = {
-                from: account,
-                to: contractAddress,
+                from: this.account,
+                to: this.contractAddress,
                 nonce: nonce,
                 value: web3.utils.toWei('0.001'),
                 gas: 500000,
                 // data: nftContract.methods.mintNFT(1).encodeABI(),
             }
-            console.log(tx);
 
             nftContract.methods.mintNFT(1).send(tx)
-            .on('transactionHash', function(hash){
+            .on('transactionHash', hash => {
                 console.log('transactionHash:'+hash);
+                this.isTransaction = 1;
+                this.transactionHash = hash;
             })
-            .on('confirmation', function(confirmationNumber, receipt){
-                console.log("confirmation", confirmationNumber, receipt);
+            .on('confirmation', (confirmationNumber, receipt) => {
+                console.log("confirmation", confirmationNumber);
+                console.log("confirmation receipt", receipt);
             })
             .on('receipt', function(receipt){
                 // receipt 相关例子
                 console.log("receipt", receipt);
+                alert('交易成功');
             })
-            .on('error', function(error, receipt) { // 如果交易被网络拒绝并带有交易收据，则第二个参数将是交易收据。
+            .on('error', (error, receipt) => { // 如果交易被网络拒绝并带有交易收据，则第二个参数将是交易收据。
                 console.log("error-----------:", error, receipt);
+                if (error.code && 4001 == error.code) {
+                    alert('取消交易');
+                } else {
+                    alert('交易失败，详情请点击交易hash值查看');
+                }
             });
         } catch (error) {
             console.log(error);
@@ -72,10 +98,29 @@ export default {
         if (!window.ethereum) {
             alert("请安装MetaMask钱包")
         }
+
+        return true;
+    },
+    showHash(value) {
+        this.isTransaction = 0;
+        this.transactionHash = value;
     }
   },
-  mounted() {
-    this.isEnableEth();
+  async mounted() {
+    this.contractAddress = process.env.VUE_APP_CONTRACT;
+    if (this.isEnableEth()) {
+        const web3 = new Web3(window.ethereum);
+        let accounts = await web3.eth.getAccounts();
+        console.log(accounts);
+        this.account = accounts[0];
+
+        let balance = await web3.eth.getBalance(this.account);
+        console.log("账户余额:"+balance)
+
+        web3.eth.getChainId().then(id => {
+            this.network = Network.NETWORK_MAP[id];
+        });
+    }
   }
 };
 </script>
